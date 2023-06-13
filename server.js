bd8c37f872dd8329bc903e862a4d7fbad7962bb5
yashrawal1998 <yash.rawal@antino.io>
2023-06-13 12:30:24 +0530

    changed index.js

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const app = express();
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', (req, res) => {
+    try {
+        return res.status(200).json({
+            status: true,
+            message: 'server running smoothly'
+        })
+    } catch (err) {
+        console.log(err)
+    }
+})
 
 app.use('/hello', (req, res) => {
     try {
@@ -17,6 +27,17 @@ app.use('/hello', (req, res) => {
     }
 })
 
+app.use('/api/world', (req, res) => {
+    try {
+        console.log('shjdsfj')
+        return res.status(200).json({
+            status: true,
+            message: 'Hello World222'
+        })
+    } catch (err) {
+        console.log(err)
+    }
+})
 
 app.listen(8080, () => {
     console.log(`server running on port 8080`)
