f11cede7415321da65f83cb405988e89929e5673
Yash <79499128+rawalYash@users.noreply.github.com>
2023-11-02 09:37:23 +0000

    fix(): config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
       name: 'app',
-      script: 'server.js', // Your entry point
+      script: './server.js', // Your entry point
       instances: 1,
       autorestart: true, // THIS is the important part, this will tell PM2 to restart your app if it falls over
       max_memory_restart: '1G'
